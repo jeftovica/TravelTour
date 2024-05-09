f5d22335474ce33174771c848a04355b41732850
jeftovica <119257123+jeftovica@users.noreply.github.com>
2024-05-09 14:23:38 +0200

    user routes

--- backend/rest/routes/user_routes.php
+++ backend/rest/routes/user_routes.php
@@ -10,18 +10,18 @@ $payload=Flight::request()->query;
 
 if($payload['email']==""){
     header('HTTP/1.1 500 Bad Request');
-    die(json_encode(['error'=>'Email is missing']));
+    die(Flight::json(['error'=>'Email is missing']));
 }
 if($payload['password']==""){
     header('HTTP/1.1 500 Bad Request');
-    die(json_encode(['error'=>'Password is missing']));
+    die(Flight::json(['error'=>'Password is missing']));
 }
 
 $user = $user_serviceFlight::get('user_service')-> login($payload);
 if($user){
-    echo json_encode(['message'=>'You have successfully Signed In', 'data'=> $user]);
+    Flight::json(['message'=>'You have successfully Signed In', 'data'=> $user]);
 }else{
     header('HTTP/1.1 500 Bad Request');
-    die(json_encode(['error'=>'Credentials are not valid']));
+    die(Flight::json(['error'=>'Credentials are not valid']));
 }
 });
